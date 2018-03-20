65bdee9e08d46996c4b795fc59f19e46b5c4d83e
Tomo Šala <hcs.omot@gmail.com>
2018-03-20 18:16:25 +0100

    Iss#18 Organization - Add Title
    Organization needs to have a title.
    
    This will add spec and code implementation for title property on
    Organization.

--- src/Organization/Entity/OrganizationEntity.php
+++ src/Organization/Entity/OrganizationEntity.php
@@ -6,9 +6,24 @@ use User\Entity\UserEntity;
 
 class OrganizationEntity
 {
+    /**
+     * @var string
+     */
+    private $title;
+
+    public function __construct(string $title)
+    {
+        $this->title = $title;
+    }
+
     /** @SuppressWarnings("PHPMD.UnusedFormalParameter") */
     public function isOrganizer(UserEntity $user): bool
     {
         //@TODO;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
     }
 }
