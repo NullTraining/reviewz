fa88285f750a0785f511b478f6a1f5fe6ebbbd9d
Miro Svrtan <miro@mirosvrtan.me>
2018-03-24 11:08:04 +0100

    Feature #24: Meetup has attendees

--- spec/Organization/Entity/MeetupEntitySpec.php
+++ spec/Organization/Entity/MeetupEntitySpec.php
@@ -4,11 +4,19 @@ namespace spec\Organization\Entity;
 
 use Organization\Entity\MeetupEntity;
 use PhpSpec\ObjectBehavior;
+use User\Entity\UserEntity;
 
 class MeetupEntitySpec extends ObjectBehavior
 {
     public function it_is_initializable()
     {
         $this->shouldHaveType(MeetupEntity::class);
+    }
+
+    public function it_will_add_attendee_to_meetup(UserEntity $attendee)
+    {
+        $this->addAttendee($attendee);
+
+        $this->getAttendees()->shouldReturn([$attendee]);
     }
 }
