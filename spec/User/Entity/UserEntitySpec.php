1ad6b53d6a8e3d0af04780107fe31eaa923c2ffa
Tomo Šala <hcs.omot@gmail.com>
2018-03-24 13:01:41 +0100

    Iss #19 User joins Organization - User has organizations
    User can be a member of one or more organizations, so it needs to keep a
    list of them.
    
    Spec+code implementation for adding an organization to a user.

--- spec/User/Entity/UserEntitySpec.php
+++ spec/User/Entity/UserEntitySpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\User\Entity;
 
+use Organization\Entity\OrganizationEntity;
 use PhpSpec\ObjectBehavior;
 use User\Entity\UserEntity;
 
@@ -10,5 +11,19 @@ class UserEntitySpec extends ObjectBehavior
     public function it_is_initializable()
     {
         $this->shouldHaveType(UserEntity::class);
+    }
+
+    public function it_should_be_able_to_have_an_organization_assigned(OrganizationEntity $organization)
+    {
+        $this->addOrganization($organization)->shouldReturn(true);
+    }
+
+    public function it_should_return_organization_assigned(OrganizationEntity $organization)
+    {
+        $this->addOrganization($organization);
+
+        $userOrganizations = $this->getOrganizations();
+
+        $userOrganizations->shouldContain($organization);
     }
 }
--- src/User/Entity/UserEntity.php
+++ src/User/Entity/UserEntity.php
@@ -2,10 +2,29 @@
 
 namespace User\Entity;
 
+use Organization\Entity\OrganizationEntity;
+
 class UserEntity
 {
+    /**
+     * @var OrganizationEntity[]
+     */
+    private $organizations;
+
     public function getName(): string
     {
         //@TODO
+    }
+
+    public function addOrganization(OrganizationEntity $organization): bool
+    {
+        $this->organizations[] = $organization;
+
+        return true;
+    }
+
+    public function getOrganizations(): array
+    {
+        return $this->organizations;
     }
 }
