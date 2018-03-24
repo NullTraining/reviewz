a75af48165565aa02b6000cdba10918f1dff9a2c
Miro Svrtan <miro@mirosvrtan.me>
2018-03-24 11:08:04 +0100

    Feature #24: Meetup has attendees

--- src/Organization/Entity/MeetupEntity.php
+++ src/Organization/Entity/MeetupEntity.php
@@ -2,10 +2,25 @@
 
 namespace Organization\Entity;
 
+use User\Entity\UserEntity;
+
 class MeetupEntity
 {
+    /** @var array|UserEntity[] */
+    private $attendees = [];
+
     public function getOrganization(): OrganizationEntity
     {
         //@TODO;
+    }
+
+    public function addAttendee(UserEntity $attendee)
+    {
+        $this->attendees[] = $attendee;
+    }
+
+    public function getAttendees(): array
+    {
+        return $this->attendees;
     }
 }
