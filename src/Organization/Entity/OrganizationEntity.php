891dd7260ba46e3f2457dcbb9b88a1f06f6bba7d
Miro Svrtan <miro@mirosvrtan.me>
2018-03-24 11:08:23 +0100

    Feature #24: Members can RSVP Yes to a meetup

--- src/Organization/Entity/OrganizationEntity.php
+++ src/Organization/Entity/OrganizationEntity.php
@@ -32,6 +32,12 @@ class OrganizationEntity
         //@TODO;
     }
 
+    /** @SuppressWarnings("PHPMD.UnusedFormalParameter") */
+    public function isMember(UserEntity $user): bool
+    {
+        //@TODO;
+    }
+
     public function getTitle(): string
     {
         return $this->title;
