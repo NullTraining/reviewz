1c6ac35abe25f2496a9da18fb29d0cda3b84f01a
tperkovic <tomislav.perkovic@skolskaknjiga.hr>
2018-03-25 09:36:31 +0200

    #21 - Added: location to event because event should have location

--- spec/Event/Entity/EventEntitySpec.php
+++ spec/Event/Entity/EventEntitySpec.php
@@ -3,14 +3,16 @@
 namespace spec\Event\Entity;
 
 use Event\Entity\EventEntity;
+use Geo\Entity\LocationEntity;
 use PhpSpec\ObjectBehavior;
 
 class EventEntitySpec extends ObjectBehavior
 {
-    public function let(\DateTime $eventDate)
+    public function let(\DateTime $eventDate, LocationEntity $location)
     {
         $this->beConstructedWith(
             $eventDate,
+            $location,
             $eventTitle = 'Event title',
             $eventDescription = 'Event description'
         );
@@ -21,10 +23,11 @@ class EventEntitySpec extends ObjectBehavior
         $this->shouldHaveType(EventEntity::class);
     }
 
-    public function it_exposes_eventdate_title_and_description(\DateTime $eventDate)
+    public function it_exposes_eventdate_title_and_description(\DateTime $eventDate, LocationEntity $location)
     {
         $this->getEventDate()->shouldReturn($eventDate);
         $this->getTitle()->shouldReturn('Event title');
         $this->getDescription()->shouldReturn('Event description');
+        $this->getLocation()->shouldReturn($location);
     }
 }
