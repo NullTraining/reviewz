2d10cb5259dbc38f72ac8c3ea39483bf20debc6f
Miro Svrtan <miro@mirosvrtan.me>
2018-04-29 12:08:18 +0200

    Feat #98: Add UserRegistered event

---
+++ src/User/Event/UserRegistered.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace User\Event;
+
+use User\Entity\UserId;
+
+class UserRegistered
+{
+    /** @var UserId */
+    private $userId;
+
+    public function __construct(UserId $userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function getUserId(): UserId
+    {
+        return $this->userId;
+    }
+}
