a42c813a8b73dd6e23e0ee69ff87af5936bc7b26
Miro Svrtan <miro@mirosvrtan.me>
2018-04-29 12:11:59 +0200

    Feat #98: UserRegistrationHandler needs EventBus

--- src/User/Hander/UserRegistrationHandler.php
+++ src/User/Hander/UserRegistrationHandler.php
@@ -2,9 +2,11 @@
 
 namespace User\Hander;
 
+use App\EventBus;
 use Geo\Repository\CityRepository;
 use User\Command\RegisterUser;
 use User\Entity\UserEntity;
+use User\Event\UserRegistered;
 
 class UserRegistrationHandler
 {
@@ -14,11 +16,14 @@ class UserRegistrationHandler
      * @var CityRepository
      */
     private $cityRepository;
+    /** @var EventBus */
+    private $eventBus;
 
-    public function __construct(\User\Repository\UserRepository $userRepository, CityRepository $cityRepository)
+    public function __construct(\User\Repository\UserRepository $userRepository, CityRepository $cityRepository, EventBus $eventBus)
     {
         $this->userRepository = $userRepository;
         $this->cityRepository = $cityRepository;
+        $this->eventBus       = $eventBus;
     }
 
     public function handle(RegisterUser $command)
