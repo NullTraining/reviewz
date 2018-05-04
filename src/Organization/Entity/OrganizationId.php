7554cf740828659539f62ca126646522b7549113
Miro Svrtan <miro@mirosvrtan.me>
2018-05-04 08:14:37 +0200

    Add OrganizationId

---
+++ src/Organization/Entity/OrganizationId.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Organization\Entity;
+
+use Ramsey\Uuid\Uuid;
+
+class OrganizationId
+{
+    /** @var string */
+    private $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public static function create(): OrganizationId
+    {
+        $value = Uuid::uuid4()->toString();
+
+        return new self($value);
+    }
+}
