4ddfe251486a668bc89df1a452b196240e5752ac
Miro Svrtan <miro@mirosvrtan.me>
2018-05-04 08:42:27 +0200

    Add UserRepository->load()

--- src/User/Repository/UserRepository.php
+++ src/User/Repository/UserRepository.php
@@ -3,10 +3,13 @@
 namespace User\Repository;
 
 use User\Entity\UserEntity;
+use User\Entity\UserId;
 
 interface UserRepository
 {
     public function save(UserEntity $entity);
 
     public function findByUsername(string $username): ?UserEntity;
+
+    public function load(UserId $id): UserEntity;
 }
