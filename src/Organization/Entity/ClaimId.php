97ea5f704394a8c93691a0cc1283ca221c3fc188
Miro Svrtan <miro@mirosvrtan.me>
2018-05-11 07:22:58 +0200

    Task #104: Add ClaimId

---
+++ src/Organization/Entity/ClaimId.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Organization\Entity;
+
+class ClaimId
+{
+    /** @var string */
+    private $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public static function create(): self
+    {
+        $value = \Ramsey\Uuid\Uuid::uuid4()->toString();
+
+        return new self($value);
+    }
+}
