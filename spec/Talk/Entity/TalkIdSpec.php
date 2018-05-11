289e65075a97ee44cb466ebd9e68a6e7b0fbff6c
Miro Svrtan <miro@mirosvrtan.me>
2018-05-11 07:23:12 +0200

    Task #104: Add TalkId

---
+++ spec/Talk/Entity/TalkIdSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\Talk\Entity;
+
+use PhpSpec\ObjectBehavior;
+use Talk\Entity\TalkId;
+
+class TalkIdSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('d65dd599-3879-4479-8032-9f17abe8317d');
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(TalkId::class);
+    }
+
+    public function it_can_create_a_random_one()
+    {
+        $this->create()->shouldReturnAnInstanceOf(TalkId::class);
+    }
+}
