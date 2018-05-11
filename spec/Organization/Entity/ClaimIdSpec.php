ebff76292a6c78109ffced46bd36bf51f65e1f08
Miro Svrtan <miro@mirosvrtan.me>
2018-05-11 07:22:58 +0200

    Task #104: Add ClaimId

---
+++ spec/Organization/Entity/ClaimIdSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\Organization\Entity;
+
+use Organization\Entity\ClaimId;
+use PhpSpec\ObjectBehavior;
+
+class ClaimIdSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('c96e7b07-30b8-40f0-a16d-953c4a0135ef');
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(ClaimId::class);
+    }
+
+    public function it_can_create_a_random_one()
+    {
+        $this->create()->shouldReturnAnInstanceOf(ClaimId::class);
+    }
+}
