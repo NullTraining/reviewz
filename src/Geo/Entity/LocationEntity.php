59336ea9fdd9d348d3bc6d204e56ded0b404dc0b
Miro Svrtan <miro@mirosvrtan.me>
2018-05-11 07:44:09 +0200

    Task #108: Use LocationId
    Location should not be created without id

--- src/Geo/Entity/LocationEntity.php
+++ src/Geo/Entity/LocationEntity.php
@@ -4,15 +4,23 @@ namespace Geo\Entity;
 
 class LocationEntity
 {
+    /** @var LocationId */
+    private $id;
     /** @var string */
     private $name;
     /** @var CityEntity */
     private $city;
 
-    public function __construct(string $name, CityEntity $city)
+    public function __construct(LocationId $id, string $name, CityEntity $city)
     {
         $this->name = $name;
         $this->city = $city;
+        $this->id   = $id;
+    }
+
+    public function getId(): LocationId
+    {
+        return $this->id;
     }
 
     public function getName(): string
