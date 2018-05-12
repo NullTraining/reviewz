e47fda46fcfa282a888aad36b3350c089d201f88
Miro Svrtan <miro@mirosvrtan.me>
2018-05-12 11:06:42 +0200

    Task #115: Setting up talk application context

---
+++ tests/Talk/Repository/TalkInMemoryRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Talk\Repository;
+
+use Talk\Entity\TalkEntity;
+use Talk\Entity\TalkId;
+use Talk\Repository\TalkRepository;
+
+class TalkInMemoryRepository implements TalkRepository
+{
+    /** @var array|TalkEntity[] */
+    private $list = [];
+
+    public function save(TalkEntity $entity)
+    {
+        $this->list[] = $entity;
+    }
+
+    public function loadByTitle(string $title): TalkEntity
+    {
+        foreach ($this->list as $item) {
+            if ($item->getTitle() === $title) {
+                return $item;
+            }
+        }
+    }
+
+    public function load(TalkId $id): TalkEntity
+    {
+        foreach ($this->list as $item) {
+            if ($item->getId() == $id) {
+                return $item;
+            }
+        }
+    }
+}
