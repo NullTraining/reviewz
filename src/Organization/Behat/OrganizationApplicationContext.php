b054649db0c534573a8bf1bd91777d044e0f0ac2
Miro Svrtan <miro@mirosvrtan.me>
2018-05-31 06:36:02 +0200

    Add missing declare strict types to PHP files
    While this code was created, in parallel strict type declarations were
    added as requirement on all files

--- src/Organization/Behat/OrganizationApplicationContext.php
+++ src/Organization/Behat/OrganizationApplicationContext.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Organization\Behat;
 
 use Behat\Behat\Context\Context;
--- src/Organization/Command/CreateOrganizationCommand.php
+++ src/Organization/Command/CreateOrganizationCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Organization\Command;
 
 use Geo\Entity\CityId;
